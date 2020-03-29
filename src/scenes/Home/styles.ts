13c0507e9ed7b57d7ba67f56084b503392032046
Marco Fiorito <marco@xmartlabs.com>
2020-03-28 21:59:34 -0300

    polish home

--- src/scenes/Home/styles.ts
+++ src/scenes/Home/styles.ts
@@ -2,10 +2,27 @@ import { Palette } from '../../style/Palette'
 import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
+  categoryContainer: { flex: 1, backgroundColor: 'white', paddingTop: 20 },
+  categoryTitle: { fontSize: 24, fontWeight: '700', paddingHorizontal: 20 },
   container: {
     alignSelf: 'stretch',
     backgroundColor: Palette.background,
     flex: 1,
+  },
+  headerTextContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: '700',
+    paddingHorizontal: 20,
+    color: 'white',
   },
   right: {
     color: Palette.white,
